f83e1539c6a7f7df081f2543beb0e63386584342
ranjan-sw-developer <ranjan.swdeveloper@gmail.com>
2025-08-15 15:09:37 +0530

    website created

---
+++ src/constants.js
@@ -0,0 +1,222 @@
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import sassLogo from "./assets/tech_logo/sass.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+import flutterLogo from "./assets/tech_logo/flutter.png";
+import dartLogo from "./assets/tech_logo/dart.png";
+import reduxLogo from "./assets/tech_logo/redux.png";
+import nextjsLogo from "./assets/tech_logo/nextjs.png";
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import gsapLogo from "./assets/tech_logo/gsap.png";
+import materialuiLogo from "./assets/tech_logo/materialui.png";
+import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
+import springbootLogo from "./assets/tech_logo/springboot.png";
+import nodejsLogo from "./assets/tech_logo/nodejs.png";
+import expressjsLogo from "./assets/tech_logo/express.png";
+import mysqlLogo from "./assets/tech_logo/mysql.png";
+import mongodbLogo from "./assets/tech_logo/mongodb.png";
+import firebaseLogo from "./assets/tech_logo/firebase.png";
+import cLogo from "./assets/tech_logo/c.png";
+import cppLogo from "./assets/tech_logo/cpp.png";
+import javaLogo from "./assets/tech_logo/java.png";
+import pythonLogo from "./assets/tech_logo/python.png";
+import typescriptLogo from "./assets/tech_logo/typescript.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+import postmanLogo from "./assets/tech_logo/postman.png";
+import mcLogo from "./assets/tech_logo/mc.png";
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import vercelLogo from "./assets/tech_logo/vercel.png";
+import postgreLogo from "./assets/tech_logo/postgre.png";
+import csharpLogo from "./assets/tech_logo/csharp.png";
+
+// Experience Section Logo's
+import niveusLogo from "./assets/company_logo/niveus.jpeg";
+
+// Education Section Logo's
+import amcLogo from "./assets/education_logo/amc.jpeg";
+import mitLogo from "./assets/education_logo/mit.jpeg";
+
+// Project Section Logo's
+import githubdetLogo from "./assets/work_logo/github_det.png";
+import csprepLogo from "./assets/work_logo/cs_prep.png";
+import movierecLogo from "./assets/work_logo/movie_rec.png";
+import taskremLogo from "./assets/work_logo/task_rem.png";
+import npmLogo from "./assets/work_logo/npm.png";
+import webverLogo from "./assets/work_logo/web_dig.png";
+import cmLogo from "./assets/work_logo/cm.png";
+import imagesearchLogo from "./assets/work_logo/image_search.png";
+import removebgLogo from "./assets/work_logo/remove_bg.png";
+
+export const SkillsInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "Flutter", logo: flutterLogo },
+      { name: "React JS", logo: reactjsLogo },
+      { name: "Next JS", logo: nextjsLogo },
+      { name: "React Native", logo: reactjsLogo },
+      { name: "Redux", logo: reduxLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      { name: "SASS", logo: sassLogo },
+      { name: "Tailwind CSS", logo: tailwindcssLogo },
+      { name: "Material UI", logo: materialuiLogo },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "Node JS", logo: nodejsLogo },
+      { name: "Express JS", logo: expressjsLogo },
+      { name: "MySQL", logo: mysqlLogo },
+      { name: "MongoDB", logo: mongodbLogo },
+      { name: "Firebase", logo: firebaseLogo },
+      // { name: "PostgreSQL", logo: postgreLogo },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      { name: "C", logo: cLogo },
+      { name: "C++", logo: cppLogo },
+      { name: "Java", logo: javaLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "TypeScript", logo: typescriptLogo },
+      { name: "Dart", logo: dartLogo },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Postman", logo: postmanLogo },
+      { name: "Compass", logo: mcLogo },
+      { name: "Vercel", logo: vercelLogo },
+      { name: "Netlify", logo: netlifyLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: niveusLogo,
+    role: "Cloud Specialist",
+    company: "Niveus Solutions Pvt. Ltd (Part of NTT DATA)",
+    date: "October 2024 - Present",
+    desc: "I’m a Full Stack Developer at Niveus Solutions Pvt. Ltd, where I’ve spent the past 5 years designing and delivering high-quality web and mobile solutions. Recognized as Star of the Month five times, I’ve contributed to multiple high-profile projects, collaborating with cross-functional teams to build seamless, impactful applications.",
+    skills: [
+      "Flutter",
+      "React JS",
+      "Redux",
+      "Next Js",
+      "React Native",
+      "Tailwind CSS",
+      "TypeScript",
+      "Node JS",
+      "MongoDb",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 1,
+    img: niveusLogo,
+    role: "Cloud Engineer",
+    company: "Niveus Solutions Pvt. Ltd",
+    date: "April 2022 - October 2024",
+    desc: "I worked as a Cloud Engineer at Niveus Solutions, developing high-performance web and mobile applications using Flutter, React.js, Redux, and other modern technologies. I focused on delivering scalable solutions, optimizing performance, and enhancing user experience across multiple high-impact projects.",
+    skills: [
+      "Flutter",
+      "React JS",
+      "React Native",
+      "Redux",
+      "Tailwind CSS",
+      "TypeScript",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 2,
+    img: niveusLogo,
+    role: "Cloud Associate",
+    company: "Niveus Solutions Pvt. Ltd",
+    date: "June 2020 - April 2022",
+    desc: "I worked as a Cloud Associate at Niveus Solutions, focusing on building dynamic web and mobile applications using React.js and React Native. I contributed to multiple client projects, delivering responsive, scalable, and user-friendly solutions.",
+    skills: ["React JS", "React Native", "HTML", "CSS", "JavaScript"],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: mitLogo,
+    school: "Manipal Institute of Technology, Manipal",
+    date: "July 2018 - June 2020",
+    grade: "7.2 CGPA",
+    desc: "I hold an M.Tech in Computer Network Engineering from Manipal Institute of Technology. My post-graduate studies strengthened my expertise in networking, system design, and emerging technologies, forming a strong foundation for my development career.",
+    degree: "M.Tech - Computer Network Engineering",
+  },
+  {
+    id: 1,
+    img: amcLogo,
+    school: "AMC Engineering College, Bangalore",
+    date: "July 2013 - June 2017",
+    grade: "70%",
+    desc: "I earned my B.E. in Computer Science Engineering from AMC Engineering College, Bangalore, building a strong foundation in programming, algorithms, and software development.",
+    degree: "BE - Computer Science and Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "GitHub Profile Detective",
+    description:
+      "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+    image: githubdetLogo,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+    github:
+      "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+    webapp: "https://githubprofiledetective.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "CS Prep",
+    description:
+      "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+    image: csprepLogo,
+    tags: [
+      "React JS",
+      "Node.js",
+      "MongoDB",
+      "Express",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    github: "https://github.com/codingmastr/CSPrep",
+    webapp: "https://csprep.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Movie Recommendation App",
+    description:
+      "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+    image: movierecLogo,
+    tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/codingmastr/Movie-Recommendation-App",
+    webapp: "https://movie-recommendation-app-jet.vercel.app/",
+  },
+];
